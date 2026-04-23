41f433cf36305e4114f76d25226eca859a80b903
decobot <capy@deco.cx>
2026-04-23 11:12:07 -0300

    fix-quiosque

--- sections/Institutional/quiosquesinstitutional.tsx
+++ sections/Institutional/quiosquesinstitutional.tsx
@@ -1,6 +1,4 @@
-import Component, {
-  Props,
-} from "../../components/Institutional/Quiosquesinstitutional.tsx";
+import Component, { Props } from "../../components/Institutional/Quiosquesinstitutional.tsx";
 
 /** @title Quiosqueinstitutional.tsx */
 export default function Section(props: Props) {
